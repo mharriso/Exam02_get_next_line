89b8c17d1d76b08eb519f1aed1834d096aea6eff
mharriso <mharriso@student.21-school.ru>
2021-02-06 16:53:08 +0300

    create

---
+++ get_next_line.c
@@ -0,0 +1,36 @@
+#include "get_next_line.h"
+
+static	char			*ft_joinchr(char const *line, char c, size_t len)
+{
+	char		*s;
+	size_t		i;
+
+	if (!(s = (char*)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+		s[i++] = (*line) ? *line++ : c;
+	s[i] = '\0';
+	return (s);
+}
+
+int	get_next_line(char **line)
+{
+	char		buffer;
+	int			res;
+	char		*tmp;
+	size_t		len;
+
+	if (!line || !(*line = (char *)malloc(1)))
+		return (-1);
+	**line = '\0';
+	len = 0;
+	while ((res = read(0, &buffer, 1)) > 0 && buffer != '\n')
+	{
+		if (!(tmp = ft_joinchr(*line, buffer, ++len)))
+			return -1;
+		free(*line);
+		*line = tmp;
+	}
+	return (res);
+}
